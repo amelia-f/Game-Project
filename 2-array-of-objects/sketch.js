b4d3c35d322e88ded6371ba03c8813bd800a7b67
amelia-f <ameliafortgang@gmail.com>
2019-10-25 09:05:43 -0700

    Update sketch.js

--- 2-array-of-objects/sketch.js
+++ 2-array-of-objects/sketch.js
@@ -15,13 +15,18 @@ function draw(){
 	for (let i = 0; i < balls.length; i++) {
 	    balls[i].drawBall();
       balls[i].moveBall();
+      squares[i].drawSquare();
+      squares[i].moveSquare();
 	  }
 }
 
 function keyPressed(){ //every time you push a key, make a new ball from the ball class and add it to the balls array
   let  b = new Ball(200, 200);
+  let s = new Square(30,30);
   balls.push(b);
   console.log(balls);
+  squares.push(s);
+  console.log(squares)
 }
 
 //ball class from which to create new balls with similar properties.
@@ -44,4 +49,22 @@ class Ball {
 	}
 
 
+}
+
+
+class Square {
+
+	constructor(x,y){ //every ball needs an x value and a y value
+		    this.x = x;
+    		this.y = y;
+	}
+	drawSquare(){  // draw a ball on the screen at x,y
+    		stroke(0);
+    		fill("blue");
+		    rect(this.x,this.y, 10,10);
+	}
+	moveSquare(){ //update the location of the ball, so it moves across the screen
+		this.x = this.x+1;
+		this.y = this.y+1;
+	}
 }
